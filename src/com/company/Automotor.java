03ff101d1b3d697291854bcb6ad3ea8da97e157e
AgusMoc <mocelliniagustina@gmail.com>
2021-02-13 17:28:30 -0300

    Agregado de consignas varias (fechas, cambio de propietario, etc)

--- src/com/company/Automotor.java
+++ src/com/company/Automotor.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -11,19 +12,27 @@ public class Automotor {
     protected String tipoAutomotor;
     protected String tipoUso;
     protected String patente;
+    protected LocalDate fecharegistro;
+    protected LocalDate fechaCambioDueño;
 
     List<String> tiposAutomotores = new ArrayList<String>(Arrays.asList("moto eléctrica", "auto eléctrico", "motocicleta", "automóvil a combustión", "colectivo", "utilitario", "camion"));
     List<String> tiposUsos = new ArrayList<String>(Arrays.asList("profesional","particular"));
 
 
-    public Automotor(String seccional, String patente, Persona propietario, String tipoAutomotor,String tipoUso,List<Persona> autorizados) {
+    public Automotor(String seccional, String patente, Persona propietario, String tipoAutomotor,String tipoUso,List<Persona> autorizados, LocalDate fecharegistro) {
         this.patente = patente;
         this.autorizados = autorizados;
         this.seccional = seccional;
         this.tipoAutomotor = tipoAutomotor;
         this.tipoUso = tipoUso;
         this.propietario = propietario;
+        this.fecharegistro = fecharegistro;
     }
+
+    public void cambiarDueño(Persona nuevoDueño, LocalDate fechaCambioDueño){
+        propietario = nuevoDueño;
+        fechaCambioDueño = fechaCambioDueño;
+            }
 
     public String getTipoAutomotor() {
         return tipoAutomotor;
